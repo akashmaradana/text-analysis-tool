10d10005deebccf57cbe8a8e07771244b86c5285
akashmaradana <maradanaakash@gamil.com>
2025-05-23 15:29:28 +0530

    Remove unnecessary files and directories

--- src/__init__.py
+++
@@ -1,5 +0,0 @@
-"""
-Text Summarization Service
-"""
-
-__version__ = "1.0.0" 
--- src/summarizer/__init__.py
+++
@@ -1,9 +0,0 @@
-"""
-Text summarization models and utilities
-"""
-
-from .base import BaseSummarizer
-from .abstractive import BartSummarizer
-from .extractive import BertSummarizer
-
-__all__ = ['BaseSummarizer', 'BartSummarizer', 'BertSummarizer'] 
